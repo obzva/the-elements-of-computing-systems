1a36495a48cf0466d51ef50f33c17d8d8b0357a2
obzva <dngyng1000@gmail.com>
2024-09-23 12:07:03 +0900

    chapter4

--- nand2tetris/projects/4/fill/Fill.asm
+++ nand2tetris/projects/4/fill/Fill.asm
@@ -1,11 +1,87 @@
 // This file is part of www.nand2tetris.org
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
-// File name: projects/4/Fill.asm
+// File name: projects/04/Fill.asm
 
 // Runs an infinite loop that listens to the keyboard input. 
 // When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel. When no key is pressed, 
-// the screen should be cleared.
+// i.e. writes "black" in every pixel. When no key is pressed, the
+// program clears the screen, i.e. writes "white" in every pixel.
 
-//// Replace this comment with your code.
+// initialize the addresses
+@SCREEN
+D=A
+@BLACK_ADDR
+M=D
+@WHITE_ADDR
+M=D
+
+// this is the last address of the screen
+@24576
+D=A
+@LAST
+M=D-1
+
+// this loop detects the key press
+(KBDLOOP)
+    @KBD
+    D=M
+
+    @BLACK
+	D;JGT
+
+	@WHITE
+	D;JEQ
+
+	@KBDLOOP
+	0;JMP
+
+// paint them black while pressing keys
+(BLACK)
+	// set WHITE_ADDR to starting address
+	@SCREEN
+	D=A
+	@WHITE_ADDR
+	M=D
+
+	@BLACK_ADDR
+	D=M
+	@LAST
+	D=D-M
+	@KBDLOOP
+	D;JGT
+
+	@BLACK_ADDR
+	A=M
+	M=-1
+
+	@BLACK_ADDR
+	M=M+1
+	
+	@KBDLOOP
+	0;JMP
+
+// clean the whole screen if no press
+(WHITE)
+	// set BLACK_ADDR to starting address
+	@SCREEN
+	D=A
+	@BLACK_ADDR
+	M=D
+
+	@WHITE_ADDR
+	D=M
+	@LAST
+	D=D-M
+	@KBDLOOP
+	D;JGT
+
+	@WHITE_ADDR
+	A=M
+	M=0
+
+	@WHITE_ADDR
+	M=M+1
+	
+	@WHITE
+	0;JMP
--- nand2tetris/projects/4/mult/Mult.asm
+++ nand2tetris/projects/4/mult/Mult.asm
@@ -7,4 +7,38 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// i = 0
+@i
+M=0
+
+// M[R2] = 0
+@R2
+M=0
+
+(LOOP)
+// if i - M[R1] == 0 then goto END
+@i
+D=M
+@R1
+D=D-M
+@END
+D;JEQ
+
+// M[R2] += M[R0]
+@R0
+D=M
+@R2
+M=D+M
+
+// i++
+@i
+M=M+1
+
+// iterate
+@LOOP
+0;JMP
+
+(END)
+// terminate with inifinite loop
+@END
+0;JMP
